f2dc63f424bca9b07a1cbf53c374d897626dd8e6
Donal Mongey <donalmongey@gmail.com>
2018-10-20 14:31:52 +0000

    Added edit and done button

--- todo.py
+++ todo.py
@@ -7,13 +7,15 @@ tasks = {
 		"id": 1,
 		"name": "get coffee",
 		"description": "none of that cheap stuff",
-		"is_urgent": False
+		"is_urgent": False,
+		"done": False
 	},
 	2 : {
 		"id": 2,
 		"name": "get milk",
 		"description": "in a carton",
-		"is_urgent": True
+		"is_urgent": True,
+		"done": False
 	},
 }
 
@@ -34,13 +36,50 @@ def add_task():
 			"id": next_id ,
 			"name": request.form["task"],
 			"description": request.form["description"],
-			"is_urgent": "is_urgent" in request.form
+			"is_urgent": "is_urgent" in request.form,
+			"done": "done" in request.form
+			
 			}
 
 		tasks[next_id] = task_to_add
 		
 		next_id += 1
 		return redirect("/")
+
+
+
+# @app.route("/edit/<int:id>")	
+# def show_edit(id):
+# 	return render_template("edit_task_form.html", task = tasks[id])
+	
+@app.route("/edit/<int:id>", methods = ["GET","POST"])
+def edit_task(id):
+	if request.method =="POST":
+		task_to_add = {
+			"id" : id,
+			"name": request.form["task"],
+			"description": request.form["description"],
+			"is_urgent": "is_urgent" in request.form, 
+			"done" : "done" in request.form
+			}
+		tasks[id] = task_to_add
+		return redirect("/")
+	
+	else:
+		return render_template("edit_task_form.html", task = tasks[id])
+
+@app.route("/done/<int:id>")
+def show_done(id):
+	task = tasks[id]
+	print(task)
+	if task["done"] == False:
+		task["done"] = True
+	else:
+		task["done"] = False
+	
+	return redirect("/")
+	
+
 
 
 if __name__ == "__main__":
