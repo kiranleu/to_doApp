2b9f08bfaf1984505c4034228396920acaf1533f
Kira <kira.anleu@hotmail.com>
2018-10-25 08:22:28 +0000

    Adding import os

--- .~c9_invoke_hGZ9n9.py
+++ .~c9_invoke_hGZ9n9.py
@@ -1,4 +1,4 @@
-import os
+from flask import Flask, render_template, request, redirect
 from flask import Flask, render_template, request, redirect
 
 
@@ -84,5 +84,5 @@ def show_done(id):
 
 
 
-if __name__ == "__main__":
-        app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+git remote add origin https://github.com/kiranleu/to_doApp.git
+git push -u origin master
